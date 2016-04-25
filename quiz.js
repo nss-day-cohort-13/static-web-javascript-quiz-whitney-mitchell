ed82ee79977b9e55f106556f7662e11f8611f2c7
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-04-25 13:36:44 -0500

    fixed spacing and tabs, removed unnessecary attributes, defined var charMath.

--- quiz.js
+++ quiz.js
@@ -1,5 +1,3 @@
-
-
 var growButton = document.getElementById("button");
 growButton.addEventListener("click", getInput);
 
@@ -9,8 +7,8 @@ for (var i = 0; i < growEnter.length; i++) {
 		if (event.keyCode===13) {
 			getInput();
 		}
-	})
-};
+	});
+}
 
 var treeSpecs = {
 	character: "" ,
@@ -18,8 +16,8 @@ var treeSpecs = {
 };
 
 function getInput() {
-	treeSpecs.character = document.getElementById('character').value;
-	treeSpecs.height = document.getElementById('height').value;
+	treeSpecs.character = document.getElementById("character").value;
+	treeSpecs.height = document.getElementById("height").value;
 	var char = treeSpecs.character;
 	var heightInt = treeSpecs.height;
 
@@ -38,7 +36,7 @@ function tree(treeObj) {
   //var i=1: user doesn't know about any zero-index. if i was 0, would have one line too many.
 		spaceMath = spaceMath.substr(1);
 		char += treeObj.character + treeObj.character;
-		charMath = char.substr(2);
- 		console.log(spaceMath + charMath + spaceMath);
+		var charMath = char.substr(2);
+		console.log(spaceMath + charMath + spaceMath);
 	}
 }
